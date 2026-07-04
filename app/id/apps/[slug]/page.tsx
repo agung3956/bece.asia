3129b33c6240940f7ca190ed9693440c69721107
agung3956 <agung3956@gmail.com>
2026-07-04 14:56:27 +0700

    Add Indonesian app detail page

---
+++ app/id/apps/[slug]/page.tsx
@@ -0,0 +1,16 @@
+import { AppDetailContent } from "@/components/app-detail-content";
+import { PageShell } from "@/components/page-shell";
+import { apps } from "@/data/apps";
+
+export function generateStaticParams() {
+  return apps.map((app) => ({ slug: app.slug }));
+}
+
+export default async function IndonesianAppDetailPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+  return (
+    <PageShell locale="id" currentPath={`/id/apps/${slug}`}>
+      <AppDetailContent locale="id" slug={slug} />
+    </PageShell>
+  );
+}
